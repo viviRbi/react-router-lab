aeb8489de09c258d628ed2044309c15f08751bbe
VyLe <thaovyletruong@gmail.com>
2019-12-19 15:46:36 -0600

    Added file for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import HomePage from "./homepage.component"
+import { Router, Link } from "react-router-dom"
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: this.name
+    }
+  }
+
   render() {
     return (
-        <div>
-        </div>
+      <div>
+
+        <nav>
+          <h1>National Park List</h1>
+        </nav>
+
+        <HomePage />
+      </div>
     );
   }
 }
